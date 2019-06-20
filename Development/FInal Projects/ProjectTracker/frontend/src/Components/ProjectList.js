1e66971db90e44bf153fe9e0e635200a20252292
ezevin <ehzevin@gmail.com>
2019-06-20 09:56:50 -0400

    ready to push to heroku?

--- Development/FInal Projects/ProjectTracker/frontend/src/Components/ProjectList.js
+++ Development/FInal Projects/ProjectTracker/frontend/src/Components/ProjectList.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react'
-import { Grid, Container } from 'semantic-ui-react'
+import { Grid, Container, Progress } from 'semantic-ui-react'
 import { } from 'react-router-dom'
 
 
   class ProjectList extends Component {
 
     render() {
-console.log(this.props);
+
+  const done = this.props.toDoList.filter(item => item.complete === true)
+  const list = this.props.toDoList.map(item => item)
+
+  const percent = ((done.length)/(list.length))*100
+
       return (
         <Container align="center" className="textAll">
           <Grid>
             <Grid.Column width={6}>
               <h3>{this.props.project}</h3>
             </Grid.Column>
-            <Grid.Column width={4}>
+            <Grid.Column align="left" width={4}>
+            {percent  ?
+              <Progress percent={percent} indicating/>
+             :
+             <Progress percent={0} indicating/>
+            }
 
             </Grid.Column>
             <Grid.Column width={6}>
